26667c18869ba7ffcba2ac8541d0df4bb4927339
pedro felipe <pedrofelipinho@gmail.com>
2026-06-02 15:09:05 -0300

    Inicializando o projeto do desafio Banco digital

---
+++ src/test/java/br/com/dbserver/banco_digital/BancoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dbserver.banco_digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
